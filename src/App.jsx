4afa6f7e36e3318f7fb96b18f76c4f5019566557
Ali Alshater <alialshater7@gmail.com>
2023-09-08 23:58:32 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,34 @@ import { BrowserRouter} from "react-router-dom";
 import { About , Contact , Experience , Hero , Navbar , Tech , Works, StarsCanvas } from "./components";
 
 import './index.css';
+import { useState , useEffect } from "react";
 
 const App = () => {
+  const [isMobile , setIsMobile] = useState(false);
+
+  useEffect(() =>{
+    const mediaQuery = window.matchMedia('(max-width: 767px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handelMediaQuery = (event) =>{
+      setIsMobile(event.matches);
+    }
+    mediaQuery.addEventListener('change' , handelMediaQuery);
+
+    return () =>{
+      mediaQuery.removeEventListener('change' , handelMediaQuery);
+    }
+  },[]);
 
   return (
     <BrowserRouter>
       <div className=" relative z-0 bg-primary">
         <div className=" bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
-          <Hero/>
+          <Hero isMobile={isMobile}/>
         </div>
-        <About/>
+        <About isMobile={isMobile}/>
         <Experience/>
         <Tech/>
         <Works/>
