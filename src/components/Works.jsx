fbf4aeaf6ec854aad50b9dfaa43a894ae2f0eff6
a_alshater <a_alshater@prokoders.com>
2025-02-06 16:59:27 -0800

    add real time chat app project

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,7 +7,6 @@ import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn , textVariant } from '../utils/motion';
 
-
 const ProjectCard =({ index , name , description , tags , image, source_code_link , demo}) =>{
   return <motion.div
    variants={fadeIn("up" , "spring" , index * 0.5 , 0.75)}
